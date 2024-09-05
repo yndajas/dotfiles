6ace3c912665e67bcc662e310d6169d5b74e50a6
Ynda Jas <yndajas@gmail.com>
2024-09-05 11:14:59 +0100

    Format .fzf.zsh
    - Default to double quoted strings
    - Use colon syntax for --preview-window argument

--- .fzf.zsh
+++ .fzf.zsh
@@ -15,11 +15,11 @@ source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 # Custom commands and options
 # ------------
 # respect .gitignore in CTRL + T/fzf
-export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+export FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
 FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # show preview window when searching for files using CTRL + T
-export FZF_CTRL_T_OPTS='--no-height --preview "bat --color=always {}" --preview-window "up,60%,~3"'
+export FZF_CTRL_T_OPTS="--no-height --preview 'bat --color=always {}' --preview-window up:60%:~3"
 
 # add wrapped preview of full command
 # useful if the command is too long to be shown in the list unwrapped
