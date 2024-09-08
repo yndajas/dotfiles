b7ce201096cb22b158cc730426bbe70df782d425
Ynda Jas <yndajas@gmail.com>
2024-09-08 03:26:36 +0100

    Improve fuzzy find interfaces
    - Customise design
    - Update commands and opts including to use fd for Ctrl + T, Esc + C,
      and various ** usages, and improve previews including for ** usages

--- .fzf.zsh
+++ .fzf.zsh
@@ -14,15 +14,75 @@ source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 
 # Custom commands and options
 # ------------
-# respect .gitignore in CTRL + T/fzf
-export FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
-FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_COMMAND="fd --strip-cwd-prefix --hidden --follow --exclude .git"
+export FZF_DEFAULT_OPTS="
+  --bind='ctrl-p:toggle-preview'
+  --border-label-pos 0
+  --border-label fzf
+  --border rounded
+  --color fg+:#1d1c25
+  --color bg+:#d4d5f0
+  --color hl:#be6bf7
+  --color hl+:#be6bf7
+  --color info:#636566
+  --color label:#636566
+  --color marker:#be6bf7
+  --color pointer:#be6bf7
+  --color prompt:#636566
+  --marker '▸'
+  --multi
+  --preview-window border-rounded
+  --reverse
+  --separator ''
+"
 
-export FZF_DEFAULT_OPTS="--multi --bind='ctrl-p:toggle-preview'"
+# command search
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}'
+  --preview-window down:2:wrap
+"
 
-# show preview window when searching for files using CTRL + T
-export FZF_CTRL_T_OPTS="--no-height --preview 'bat --color=always {}' --preview-window up:60%:~3"
+# file search
+file_search_preview="bat --color=always {}"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND --type f"
+export FZF_CTRL_T_OPTS="
+  --no-height
+  --preview '${file_search_preview}'
+  --preview-window down:60%:~3
+"
 
-# add wrapped preview of full command
-# useful if the command is too long to be shown in the list unwrapped
-export FZF_CTRL_R_OPTS="--reverse --preview 'echo {}' --preview-window down:2:wrap"
+# directory search
+directory_search_preview="eza --all --git-ignore --tree --level=2 --classify --color=always --icons=always --sort=type {}"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND --type d"
+export FZF_ALT_C_OPTS="--preview '${directory_search_preview}'"
+
+# ** search
+
+## what search command to use
+
+### for paths
+
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude .git . "$1"
+}
+
+### for directories
+
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude .git . "$1"
+}
+
+## how to preview based on preceding command
+
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    kill|unalias) fzf;;
+    export|unset) fzf "$@" --preview "eval 'echo $'{}";;
+    ssh)          fzf "$@" --preview "dig {}";;
+    cd)           fzf "$@" --preview "${directory_search_preview}";;
+    *)            fzf "$@" --no-height --preview "[ -f {} ] && ${file_search_preview} || ${directory_search_preview}" --preview-window down:60%:~3;;
+  esac
+}
