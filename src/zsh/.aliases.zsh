9283aedc9e4f377ad73ed4f4502b12684ba4499d
Ynda Jas <yndajas@gmail.com>
2025-05-27 23:39:21 +0100

    Remove unused aliases

--- src/zsh/.aliases.zsh
+++ src/zsh/.aliases.zsh
@@ -4,24 +4,6 @@
 
 alias cat="bat"
 
-# directories/paths
-# zoxide is aliased to cd using `zoxide init zsh --cmd cd` in .zshrc
-
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-alias -g ......='../../../../..'
-
-alias 1='cd -'
-alias 2='cd -2'
-alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
-alias 6='cd -6'
-alias 7='cd -7'
-alias 8='cd -8'
-alias 9='cd -9'
-
 # ls -> eza
 
 alias ls="eza --all --classify --color=always --icons=always --sort=type"
