5ccaf65a31108c71ebf340bceb134de2c7ae1fe8
Ynda Jas <yndajas@gmail.com>
2025-05-29 00:05:50 +0100

    Update dotfiles function to use 'commands'
    In a similar way to git. This also absorbs the install_dotfiles function
    into the main dotfiles function
    
    The item in array checks are quite different to Bash. These helped:
    - https://unix.stackexchange.com/a/411331/639529
    - https://zsh.sourceforge.io/Doc/Release/Parameters.html#Subscript-Flags
    
    read is also different, with the `-p` flag not being for prompts.
    Instead you need to use a '?' as a separator in the argument:
    - https://zsh.sourceforge.io/Doc/Release/Shell-Builtin-Commands.html#index-read
    - https://stackoverflow.com/a/15174634/4002016
    
    This is using some complex inline conditionals that I learned from bash
    Idioms

--- src/zsh/.config/dotfiles.zsh
+++ src/zsh/.config/dotfiles.zsh
@@ -1,33 +1,38 @@
 #!/usr/bin/env zsh
 
-# open dotfiles in VSCode by default or optionally specify whether you want to
-# do that and/or change directory ----------------------------------------------
 function dotfiles() {
-  [[ $# -eq 0 ]] && code $DOTFILES_DIR
+  local command=''
+  local valid_commands=(cd code install)
+  local usage_text="usage: dotfiles <command>
 
-  while [[ $# -gt 0 ]]; do
-    case $1 in
-      --cd) cd $DOTFILES_DIR;;
-      --code) code $DOTFILES_DIR;;
-    esac
+Commands:
+   cd         change into the directory of the dotfiles repository
+   code       open the dotfiles repository in VS Code
+   install    reinstall the dotfiles (idempotent)"
 
-    shift
+  [[ $# -gt 1 ]] && echo $usage_text && return 1
+
+  if [[ $# -eq 1 ]]; then
+    (( $valid_commands[(Ie)$1] )) && command=$1 || { echo $usage_text && return 1 }
+  fi
+
+  while ! (( $valid_commands[(Ie)$command] )); do
+    read 'command?Enter command (cd/code/install): '
   done
-} # ----------------------------------------------------------------------------
 
-function install_dotfiles() {
-  eval $DOTFILES_DIR/install
+  case $command in
+    cd) cd $DOTFILES_DIR;;
+    code) code $DOTFILES_DIR;;
+    install) eval $DOTFILES_DIR/install;;
+  esac
 }
 
-# alert if dotfiles changes are not committed or pushed to remote --------------
+# alert if dotfiles changes are not committed or pushed to remote
 source $HOME/.config/zsh/text_formatting.zsh
-
 if [[ -n "$(eval git -C $DOTFILES_DIR status --porcelain)" ]]; then
   set_text_format --foreground red
   echo "Warning: uncommitted changes in $DOTFILES_DIR"
-else
-  if [[ -n "$(eval git -C $DOTFILES_DIR diff @{u})" ]]; then
-    set_text_format --foreground red
-    echo "Warning: unpushed changes in $DOTFILES_DIR"
-  fi
-fi # ---------------------------------------------------------------------------
+elif [[ -n "$(eval git -C $DOTFILES_DIR diff @{u})" ]]; then
+  set_text_format --foreground red
+  echo "Warning: unpushed changes in $DOTFILES_DIR"
+fi
