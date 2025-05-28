93b8956a400a58b4b8a8f945686da943e64a7330
Ynda Jas <yndajas@gmail.com>
2025-05-28 16:12:14 +0100

    Align items in associative array

--- src/zsh/.config/text_formatting.zsh
+++ src/zsh/.config/text_formatting.zsh
@@ -5,26 +5,26 @@ bold_code=1
 
 declare -A foreground_codes
 foreground_codes=(
-  black 30 bright-black 90
-  red 31 bright-red 91
-  green 32 bright-green 92
-  yellow 33 bright-yellow 93
-  blue 34 bright-blue 94
+  black   30 bright-black   90
+  red     31 bright-red     91
+  green   32 bright-green   92
+  yellow  33 bright-yellow  93
+  blue    34 bright-blue    94
   magenta 35 bright-magenta 95
-  cyan 36 bright-cyan 96
-  white 37 bright-white 97
+  cyan    36 bright-cyan    96
+  white   37 bright-white   97
 )
 
 declare -A background_codes
 background_codes=(
-  black 40 bright-black 100
-  red 41 bright-red 101
-  green 42 bright-green 102
-  yellow 43 bright-yellow 103
-  blue 44 bright-blue 104
+  black   40 bright-black   100
+  red     41 bright-red     101
+  green   42 bright-green   102
+  yellow  43 bright-yellow  103
+  blue    44 bright-blue    104
   magenta 45 bright-magenta 105
-  cyan 46 bright-cyan 106
-  white 47 bright-white 107
+  cyan    46 bright-cyan    106
+  white   47 bright-white   107
 )
 
 function set_text_format() {
