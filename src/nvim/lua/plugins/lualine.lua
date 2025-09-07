6f4a3c30cc9c0e897b793a292e01cb3e3256c1bf
Ynda Jas <yndajas@gmail.com>
2025-09-08 00:46:06 +0100

    Show relative path in lualine
    By default it just shows the file name

--- src/nvim/lua/plugins/lualine.lua
+++ src/nvim/lua/plugins/lualine.lua
@@ -4,6 +4,17 @@ return {
     dependencies = {
         "nvim-tree/nvim-web-devicons",
      },
-    opts = {},
+    config = function()
+      require("lualine").setup {
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            }
+          }
+        }
+      }
+    end,
   },
 }
