ed73e5af36a40e86810c1524fbae546ee33449ad
Ynda Jas <yndajas@gmail.com>
2025-09-19 00:56:11 +0100

    Set Neovim leader to space

--- src/nvim/init.lua
+++ src/nvim/init.lua
@@ -1,3 +1,6 @@
+-- set leader key to space rather than backslash; needs to come before Lazy
+vim.g.mapleader = " "
+
 require("config.options") -- core (Neo)vim options
 require("config.lazy") -- package management
 
