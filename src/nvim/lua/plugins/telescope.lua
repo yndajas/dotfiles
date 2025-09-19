270c03e15194e4240b4073291e3eba735d024ac4
Ynda Jas <yndajas@gmail.com>
2025-09-19 01:52:42 +0100

    Add descriptions to Telescope keymaps

--- src/nvim/lua/plugins/telescope.lua
+++ src/nvim/lua/plugins/telescope.lua
@@ -44,11 +44,17 @@ return {
               },
             }
           )
-        end, {}
+        end, { desc = "Find file with fd" }
       )
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
+        desc = "Grep files"
+      })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+        desc = "Find buffer"
+      })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
+        desc = "Find help" }
+      )
     end,
   },
 }
