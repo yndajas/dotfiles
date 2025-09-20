b92e866a6c994316b0f75410ad80ae96c64a0166
Ynda Jas <yndajas@gmail.com>
2025-09-20 01:08:38 +0100

    Add keymaps for changing buffer
    ctrl-tab and ctrl-shift-tab don't work, so these omit the ctrl
    
    https://www.reddit.com/r/neovim/comments/11pw5cu/mapping_ctrltab_and_ctrlshifttab

--- src/nvim/lua/config/keymap.lua
+++ src/nvim/lua/config/keymap.lua
@@ -23,3 +23,7 @@ vim.keymap.set("v", "<M-Up>", ":move '<-2<CR>gv=gv")
 vim.keymap.set("v", "<M-Down>", ":move '>+1<CR>gv=gv")
 vim.keymap.set("n", "<M-Up>", "<S-v> :move '<-2<CR><S-v>=")
 vim.keymap.set("n", "<M-Down>", "<S-v> :move '>+1<CR><S-v>=")
+
+-- alias tab and shift-tab to buffer next/previous
+vim.keymap.set("n", "<Tab>", ":bnext<CR>")
+vim.keymap.set("n", "<S-Tab>", ":bprev<CR>")
