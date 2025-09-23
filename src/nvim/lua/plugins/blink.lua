1e2a647e556b3ca230ce426209ec0492e1fe0052
Ynda Jas <yndajas@gmail.com>
2025-09-23 21:21:23 +0100

    Add completions for Neovim config

--- src/nvim/lua/plugins/blink.lua
+++ src/nvim/lua/plugins/blink.lua
@@ -2,6 +2,18 @@ return {
   {
     "saghen/blink.cmp",
     version = "1.*",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            -- load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
     opts = {
       completion = {
         documentation = {
@@ -30,6 +42,17 @@ return {
       },
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
+        per_filetype = {
+          lua = { inherit_defaults = true, "lazydev" },
+        },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
       },
     },
   },
