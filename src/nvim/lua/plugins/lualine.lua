019eefce4e41e50c7d8178d3749a9d9a7f9ec7ff
Ynda Jas <yndajas@gmail.com>
2025-09-23 19:42:10 +0100

    Update Lualine config
    - show buffers as tabs in 'tabline'
    - refresh the status line, removing various components and adding LSP
      information

--- src/nvim/lua/plugins/lualine.lua
+++ src/nvim/lua/plugins/lualine.lua
@@ -2,17 +2,29 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     dependencies = {
-        "nvim-tree/nvim-web-devicons",
+      "nvim-tree/nvim-web-devicons",
     },
     opts = {
-      sections = {
-        lualine_c = {
+      tabline = {
+        lualine_a = {
           {
-            'filename',
-            path = 1,
-          }
-        }
-      }
-    }
-  },
+            "buffers",
+            icons_enabled = false,
+            component_separators = { left = "|", right = "" },
+            max_length = vim.o.columns,
+            mode = 4,
+            section_separators = { left = "", right = "" },
+          },
+        },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {},
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "diagnostics" },
+        lualine_y = { { "lsp_status", icons_enabled = false } },
+        lualine_z = { { "location", padding = 0 } },
+      },
+    },
+  }
 }
