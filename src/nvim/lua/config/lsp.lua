9624d953b5163177af39b079a5be4989cc0f3d6b
Ynda Jas <yndajas@gmail.com>
2025-12-02 00:50:48 +0000

    Fix Ruby LSP command
    Without this, it was defaulting to running on some global version of
    Ruby (but I don't think 'system' Ruby, nor the rbenv's global version),
    and when that didn't match the version used by whatever project was open
    in Vim, Ruby LSP broke
    
    This is a common fix, e.g.:
    - https://github.com/mason-org/mason.nvim/issues/1777 (not exactly the
      same)
    - https://github.com/Shopify/ruby-lsp/issues/2347#issuecomment-2308784215
    - https://shopify.github.io/ruby-lsp/editors.html#mason
    - https://github.com/wengtadashi/dotfiles/blob/f33f2eac229e5cb1e6c6b0b67c91b0f8541827a5/.config/lazyvim/lua/plugins/ruby-lsp.lua#L8

--- src/nvim/lua/config/lsp.lua
+++ src/nvim/lua/config/lsp.lua
@@ -15,5 +15,8 @@ vim.keymap.set(
   { desc = "Run an LSP action in command mode" }
 )
 
+vim.lsp.config("ruby_lsp", {
+  cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") }
+})
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("ruby_lsp")
