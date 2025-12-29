20323c2df988e6081943fcf9742279a43780a501
Ynda Jas <yndajas@gmail.com>
2025-12-29 13:15:13 +0000

    Format Telescope config
    ... using whatever Lua formatter I added to Vim via Conform

--- src/nvim/lua/plugins/telescope.lua
+++ src/nvim/lua/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
     },
     config = function()
       require("telescope").load_extension("fzf")
-      require("telescope").setup {
+      require("telescope").setup({
         defaults = {
           layout_strategy = "vertical",
         },
@@ -19,56 +19,56 @@ return {
           buffers = {
             mappings = {
               i = {
-                ['<M-d>'] = require('telescope.actions').delete_buffer
+                ["<M-d>"] = require("telescope.actions").delete_buffer,
               },
               n = {
-                ['<M-d>'] = require('telescope.actions').delete_buffer
-              }
-            }
+                ["<M-d>"] = require("telescope.actions").delete_buffer,
+              },
+            },
           },
           find_files = {
             hidden = true,
             no_ignore = false,
           },
         },
-      }
+      })
 
       vim.keymap.set("n", "<leader>t", ":Telescope ")
 
       local builtin = require("telescope.builtin")
-      vim.keymap.set(
-        "n", "<leader>fff", function()
-          builtin.find_files(
-            {
-              find_command = {
-                "fd",
-                "--strip-cwd-prefix",
-                "--hidden",
-                "--follow",
-                "--exclude",
-                ".git",
-                "--type",
-                "f",
-              },
-            }
-          )
-        end, { desc = "Find file with fd" }
-      )
+      vim.keymap.set("n", "<leader>fff", function()
+        builtin.find_files({
+          find_command = {
+            "fd",
+            "--strip-cwd-prefix",
+            "--hidden",
+            "--follow",
+            "--exclude",
+            ".git",
+            "--type",
+            "f",
+          },
+        })
+      end, { desc = "Find file with fd" })
       -- exclude git submodules (by default), unlike fd
       vim.keymap.set("n", "<leader>ffg", function()
         builtin.git_files({ show_untracked = true })
       end, { desc = "Find file with git" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
-        desc = "Grep files"
+        desc = "Grep files",
       })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {
-        desc = "Find buffer"
+        desc = "Find buffer",
       })
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
-        desc = "Find help" }
-      )
+        desc = "Find help",
+      })
       vim.keymap.set("n", "<leader>fd", function()
-        builtin.diagnostics({ bufnr = 0, sort_by = "severity", wrap_results = true })
+        builtin.diagnostics({
+          bufnr = 0,
+          sort_by = "severity",
+          wrap_results = true,
+        })
       end, { desc = "Find diagnostics" })
     end,
   },
