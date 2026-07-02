5c8ab4439fa628a96e63f8d06843f5acc945501b
Ynda Jas <yndajas@gmail.com>
2026-07-03 00:58:38 +0100

    Add TODO for Neovim diagnostics

--- src/nvim/lua/config/diagnostics.lua
+++ src/nvim/lua/config/diagnostics.lua
@@ -1,5 +1,7 @@
 vim.diagnostic.config({
   severity_sort = true,
+  -- TODO: consider switching to just unconditional virtual lines - virtual text
+  -- looks nice but it doesn't play well with half a small screen
   virtual_text = true,
   virtual_lines = { current_line = true },
 })
