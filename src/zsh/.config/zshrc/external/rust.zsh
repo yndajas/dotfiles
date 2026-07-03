d307fc2443caefbb689b60a93fa013007021fa29
Ynda Jas <yndajas@gmail.com>
2026-07-03 01:59:00 +0100

    Move subshell out of variable export
    This raised https://www.shellcheck.net/wiki/SC2155
    
    "Declare and assign separately to avoid masking return values."
    
    The issue here is including a subshell directly in the assignment string

--- src/zsh/.config/zshrc/external/rust.zsh
+++ src/zsh/.config/zshrc/external/rust.zsh
@@ -1,4 +1,7 @@
 #!/usr/bin/env zsh
 
-path_excludes "$(brew --prefix rustup)/bin" && export PATH="${PATH}:$(brew --prefix rustup)/bin"
+rustup_dir="$(brew --prefix rustup)"
+path_excludes "${rustup_dir}/bin" && export PATH="${PATH}:${rustup_dir}/bin"
+unset rustup_dir
+
 path_excludes "${HOME}/.cargo/bin" && export PATH="${PATH}:${HOME}/.cargo/bin"
